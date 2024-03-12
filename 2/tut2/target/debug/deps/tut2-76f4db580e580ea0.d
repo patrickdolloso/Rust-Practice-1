db615e85c9c7e3ef26b11d85efdb3d030a7610c9
Patrick Dolloso <patrick.dolloso@gmail.com>
2024-03-12 02:06:05 +0000

    folder 2

---
+++ 2/tut2/target/debug/deps/tut2-76f4db580e580ea0.d
@@ -0,0 +1,5 @@
+/workspaces/Rust-Practice-1/2/tut2/target/debug/deps/tut2-76f4db580e580ea0: src/main.rs
+
+/workspaces/Rust-Practice-1/2/tut2/target/debug/deps/tut2-76f4db580e580ea0.d: src/main.rs
+
+src/main.rs:
---
+++ 2/tut2/target/debug/tut2.d
@@ -0,0 +1,1 @@
+/workspaces/Rust-Practice-1/2/tut2/target/debug/tut2: /workspaces/Rust-Practice-1/2/tut2/src/main.rs
