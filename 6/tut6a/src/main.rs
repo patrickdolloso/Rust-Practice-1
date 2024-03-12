3ed597f134dcff4928341e986dfc2be746df79b4
Patrick Dolloso <patrick.dolloso@gmail.com>
2024-03-12 21:10:41 +0000

    folder 6

--- 6/tut6a/src/main.rs
+++ 6/tut6a/src/main.rs
@@ -1,3 +1,12 @@
+// Tut 6: Arithmetic
+
+/* 
+convert string to int
+*/
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("error");
+    let int_input: i64 = input.trim().parse().unwrap();
+    println!("{}", int_input + 4);
 }
