69e35dfa6576e1cd647af0b5dbab7420b15f0e25
Patrick Dolloso <patrick.dolloso@gmail.com>
2024-03-12 03:35:42 +0000

    folder 4

--- 4/tut4a/src/main.rs
+++ 4/tut4a/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // tuple
+    let mut tup: (i8, bool, char) = (1, true, 's');
+    println!("{}, {}, {}", tup.0, tup.1, tup.2);
+    // mutable
+    tup.0 = 10;
+    println!("{}, {}, {}", tup.0, tup.1, tup.2);
+
+
+    // arrays
+    let mut arr = [2, 0, 4, 4];
+    arr[3] = 3;
+    println!("{}", arr[3]);
+
 }
